52c4b624ae96416cf663b7fb46f05764ce9fb9ce
nickelsh1ts <nicholas@nickelsh1ts.com>
2024-07-21 00:13:16 -0400

    build(help): getting started topics

--- src/app/help/(topics)/getting-started/quick-start/page.tsx
+++ src/app/help/(topics)/getting-started/quick-start/page.tsx
@@ -1,4 +1,51 @@
 import Breadcrumbs from '@app/components/Help/Breadcrumbs';
+import HelpCard from '@app/components/Help/HelpCard';
+
+const anchors = [{
+  href: '#first',
+  title: 'First step'
+},{
+  href: '#second',
+  title: 'Second Step'
+},{
+  href: '#third',
+  title: 'Third Step'
+},{
+  href: '#last',
+  title: 'Last step'
+}]
+
+const HelpContent = () => {
+  return(
+    <>
+      <div className="mt-5 font-extrabold" id="first">
+        First things first
+      </div>
+      <p className='ms-14 my-4'>
+        Once you&apos;ve created your Ple<span className='text-accent'>x</span>&trade; account and registered for <span className='text-primary font-bold'>Streamarr</span> you&apos;ll receive an invite email that requires you to accept.
+      </p><p className="italic text-sm my-4"><span className='text-error font-bold'>Important Information</span>: Until you accept this invite, your Ple<span className='text-accent'>x</span>&trade; account will not have access to <span className='text-primary font-bold'>Streamarr</span> content.</p>
+      <div className="mt-5 font-extrabold" id="second">
+        Next, we can login and start setting up your account
+      </div>
+      <p className='ms-14 my-4'>
+        You&apos;ll notice when signing in for the first time, that the Home page currently lists a lot of content available from Ple<span className='text-accent'>x</span>&trade; and not <span className='text-primary font-bold'>Streamarr</span>. To change this, you need to log into app.ple<span className='text-accent'>x</span>.tv and &quot;pin&quot; <span className='text-primary font-bold'>Streamarr</span> via the menu.
+      </p><p className="italic text-sm mt-4"><span className='text-info font-bold'>Tip</span>: You can also reorder the libraries so they appear on the home page in your desired order.</p>
+      <p className="italic text-sm mb-4">Our recommended order is: Movies, TV Shows, Retro: Movies, Retro: TV Shows, Retro: Kids Shows, Discover, Watchlist</p>
+      <div className="mt-5 font-extrabold" id="third">
+        And if we only want to see & access <span className='text-primary font-bold'>Streamarr</span> content, not Ple<span className='text-accent'>x</span>&trade;
+      </div><p className='ms-14 my-4'>
+        From the Options menu in <span className='text-primary font-bold'>Streamarr</span> you can select Online Media Sources and disable Live TV, Movies & TV and Music.
+      </p>
+      <p className="italic text-sm my-4">Optionally: you can adjust your Discovery preferences here as well. We recommend leaving these as their default though.</p>
+      <div className="mt-5 font-extrabold" id="last">
+        That&apos;s it! Now we can watch and request to our hearts content.
+      </div><p className='ms-14 my-4'>
+        Browse the library, download the apps, and make requests via Overseerr.
+      </p>
+      <p className="italic text-sm my-4"><span className='text-error font-bold'>Remember</span>: You won&apos;t be able to invite friends during your 5 day trial period.</p>
+    </>
+  )
+}
 
 const QuickStart = () => {
   return (
@@ -8,7 +55,7 @@ const QuickStart = () => {
         homeElement={'Help Centre'}
         names="Getting Started,Quick Start Guide"
       />
-      <div>QuickStart</div>
+      <HelpCard heading={'Quick Start Guide'} subheading={'We know there can be quite a few steps to sign up and it can get quite confusing, so below is a comprehensive guide to get you going asap.'} anchors={anchors} content={<HelpContent />} />
     </section>
   );
 };
--- src/app/help/(topics)/getting-started/what-is-streamarr/page.tsx
+++ src/app/help/(topics)/getting-started/what-is-streamarr/page.tsx
@@ -1,4 +1,5 @@
 import Breadcrumbs from '@app/components/Help/Breadcrumbs';
+import HelpCard from '@app/components/Help/HelpCard';
 import type { Metadata } from 'next';
 
 const applicationTitle = 'Streamarr';
@@ -6,6 +7,19 @@ const applicationTitle = 'Streamarr';
 const messages = {
   title: 'Help Centre',
 };
+
+const HelpContent = () => {
+  return (
+    <>
+      <div className=''>
+      <p className='mb-4'><span className='text-primary font-bold'>Streamarr</span> is a free private members only streaming service build on top of an ecosystem of free and open source applications such as Sonarr, Radarr, Overserr, and Plex!</p>
+      <p className='mb-4'>Stream almost anything from anywhere at anytime, for free. If it&apos;s not already available, simply request it. Offering an extremely wide array of content already, and backed by member initiated requests.</p>
+      <p className='mb-4'>Built in NextJS and running on Node, <span className='text-primary font-bold'>Streamarr</span> is an open sourced application designed to work in tandem and on top of other famously open sourced applications.</p>
+      <p className='mmm-4'>It&apos;s purpose is to supplement and compliment those applications that allows users to seamlessly interact and use each one in a manner that requires no technical skill and make for an enjoyable experience.</p>
+      </div>
+    </>
+  )
+}
 
 export const metadata: Metadata = {
   title: `${messages.title} - ${applicationTitle}`,
@@ -19,7 +33,7 @@ const WhatIsStreamarr = () => {
         homeElement={'Help Centre'}
         names="Getting Started,What is Streamarr?"
       />
-      <div>WhatIsStreamarr</div>
+      <HelpCard heading='What is Streamarr?' subheading='Been wondering to yourself, "what the heck is Streamarr"? Look no further...' content={<HelpContent />} />
     </section>
   );
 };
