732f4982f49fa21e80b526a4f7896e3030dec220
nickelsh1ts <nicholas@nickelsh1ts.com>
2024-08-02 21:17:58 -0400

    fix: updated loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <div className="animate-spin h-12 w-12 border-t-4 border-primary rounded-full" />
+      <span className="text-lg text-white me-1">Loading</span> <span className="loading loading-dots loading-md text-primary mt-2"></span>
     </div>
   );
 }
