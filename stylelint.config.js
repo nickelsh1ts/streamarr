5f59f36be4b2c6f14c44525128ba76077b7040c5
nickelsh1ts <nicholas@nickelsh1ts.com>
2024-08-26 04:37:55 -0400

    fix: stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['apply', 'variants', 'responsive', 'screen', 'layer'],
-      },
-    ],
+    'at-rule-no-unknown': null,
     'no-descending-specificity': null,
   },
 };
