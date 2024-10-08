ed60813ea5ed961ccd036c5b38fe404cc69d3dcc
nickelsh1ts <nicholas@nickelsh1ts.com>
2024-10-07 22:54:55 -0400

    fix(sched): added missing page title

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,4 +1,15 @@
 import BigCalendar from '@app/components/Common/BigCalendar';
+import type { Metadata } from 'next';
+
+const applicationTitle = 'Streamarr';
+
+const messages = {
+  title: 'Schedule',
+};
+
+export const metadata: Metadata = {
+  title: `${messages.title} - ${applicationTitle}`,
+};
 
 const SchedulePage = () => {
   return (
