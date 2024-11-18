44db88fafc0b726748163fb595de2e937c06c169
nickelsh1ts <nicholas@nickelsh1ts.com>
2024-11-17 20:32:39 -0500

    fix: undefined style error in loading ellipsis

--- src/components/Common/LoadingEllipsis/index.tsx
+++ src/components/Common/LoadingEllipsis/index.tsx
@@ -16,7 +16,7 @@ export const SmallLoadingEllipsis = ({ fixed }: LoadingProps) => {
 const LoadingEllipsis = ({ fixed }: LoadingProps) => {
   return (
     <div
-      className={`${fixed && 'fixed '}inset-0 flex items-center justify-center text-primary-content text-lg gap-1`}
+      className={`${fixed ? 'fixed ' : ''}inset-0 flex items-center justify-center text-primary-content text-lg gap-1`}
     >
       Loading
       <span className="loading loading-dots loading-md text-primary mt-2"></span>
