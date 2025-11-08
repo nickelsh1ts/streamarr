2c59677d8cae93fb61b01d40e9cde7deabe841c7
nickelsh1ts <nicholas@nickelsh1ts.com>
2025-11-08 02:01:32 -0500

    fix: local user signUp header issue

--- src/components/SignUp/Forms/SignUpAuthForm.tsx
+++ src/components/SignUp/Forms/SignUpAuthForm.tsx
@@ -80,6 +80,7 @@ const SignUpAuthForm = ({
     setLoading(true);
     try {
       const response = await axios.post<{
+        success: boolean;
         user: User;
         message?: string;
       }>('/api/v1/signup/localauth', {
