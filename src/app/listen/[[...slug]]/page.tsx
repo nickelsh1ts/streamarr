315570f436b3630648534e18f43845d95463b091
Nicholas Wege <nicholas@nickelsh1ts.com>
2026-09-04 00:14:18 -0400

    feat(styles): add Audiobookshelf styling (#600)

--- src/app/listen/[[...slug]]/page.tsx
+++ src/app/listen/[[...slug]]/page.tsx
@@ -1,11 +1,16 @@
 import Listen from '@app/components/Listen';
+import { withVersion } from '@app/utils/assetVersion';
 import { generatePageMetadata } from '@app/utils/serverFetchHelpers';
 import type { NextPage } from 'next';
 
 export const generateMetadata = () => generatePageMetadata('Listen');
 
 const ListenPage: NextPage = () => {
-  return <Listen />;
+  return (
+    <Listen>
+      <link rel="stylesheet" href={withVersion('/listen.css')} />
+    </Listen>
+  );
 };
 
 export default ListenPage;
